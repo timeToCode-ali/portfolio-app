cb02cbbc34fff37a17ed32396dc20943ca691f32
Joel Carr <JoelCarr619@gmail.com>
2023-06-13 16:33:15 -0700

    initial commit

---
+++ app/components/header.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export const Header = () => {
+  return <div>Header</div>
+}
+
---
+++ app/my-projects/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const MyProjects = () => {
+  return <div>MyProjects</div>
+
+}
+
+export default MyProjects
